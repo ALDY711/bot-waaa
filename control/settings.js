c1c636664c13f51eebe4bc22e633e8c809892c06
ALDY711 <107632350+ALDY711@users.noreply.github.com>
2026-09-15 12:34:02 +0700

    feat: add global configuration settings for bot owner and session

--- control/settings.js
+++ control/settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 // settings bot
 global.namaown = "ALDY"
-global.owner = ["6281233959931"]
-global.botNumber = "6281233959931" // Nomor WhatsApp bot untuk pairing code otomatis di Railway / VPS (format: 628xxx tanpa +)
+global.owner = ["62881026633190"]
+global.botNumber = "62881026633190" // Nomor WhatsApp bot untuk pairing code otomatis di Railway / VPS (format: 628xxx tanpa +)
 global.session = "session"
 
